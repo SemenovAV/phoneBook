8d6b8dc5f271d3b8f4963973462e31609b4db57a
Aleksey Semenov <7.on.off@bk.ru>
2020-03-08 12:17:32 +0300

    Реализация.
    Реализует каркас телефонной книги.

---
+++ phone_book.py
@@ -0,0 +1,18 @@
+class PhoneBook:
+    def __init__(self, book_title):
+        pass
+
+    def view_contacts(self):
+        pass
+
+    def add_contact(self):
+        pass
+
+    def del_contact(self):
+        pass
+
+    def search_additional(self):
+        pass
+
+    def search_for_name(self):
+        pass
