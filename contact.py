01daa8f704bdcc1fb9742daff6197880b237b229
Aleksey Semenov <7.on.off@bk.ru>
2020-03-08 12:09:22 +0300

    Реализация.
    Реализует класс Contact.

--- contact.py
+++ contact.py
@@ -1,16 +1,18 @@
 class Contact:
-    def __init__(self, first_name, last_name, phone_number, favourites=False, *args, **kwargs):
+    def __init__(self, first_name, last_name, phone_number, *args, favourites=False, **kwargs):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.favourites = favourites
+        self.additional_info = kwargs
+        self.additional_phone = args
 
     def __str__(self):
+        divider = '\n'
+        tab = '\t'
         return (f'Имя: {self.first_name}\n'
                 f'Фамилия: {self.last_name}\n'
                 f'Телефон: {self.phone_number}\n'
-                f'В избранных: {"да" if self.favourites else "нет"}')
-
-
-jhon = Contact('Jhon', 'Smith', '+71234567809')
-print(jhon)
+                f'Дополнительные номера:\n{divider.join([f"{tab} {phone}" for phone in self.additional_phone])}\n'
+                f'В избранных: {"да" if self.favourites else "нет"}\n'
+                f'Дополнительная информация:\n{divider.join([f"{tab} {key}: {value}" for key, value in self.additional_info.items()])}')
