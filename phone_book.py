56b51dbfaa12bea509a3377f410cc54e7ee2115f
Aleksey Semenov <7.on.off@bk.ru>
2020-03-08 12:47:03 +0300

    Реализация.
    Реализует требуемые методы телефонной книги.

--- phone_book.py
+++ phone_book.py
@@ -1,18 +1,24 @@
+from contact import Contact
+
+
 class PhoneBook:
     def __init__(self, book_title):
-        pass
+        self.title = book_title
+        self.book = set()
 
     def view_contacts(self):
-        pass
+        for contact in self.book:
+            print(contact)
 
-    def add_contact(self):
-        pass
+    def add_contact(self, contact):
+        if isinstance(contact, Contact):
+            self.book.add(contact)
 
-    def del_contact(self):
-        pass
+    def del_contact(self, number):
+        self.book.remove(filter(lambda x: x['phone_number'] == number, self.book))
 
-    def search_additional(self):
-        pass
+    def search_favourites(self):
+        return filter(lambda x: x['favourites'], self.book)
 
-    def search_for_name(self):
-        pass
+    def search_for_name(self, first_name, last_name):
+        return filter(lambda x: x['first_name'] == first_name and x['last_name'] == last_name, self.book)
